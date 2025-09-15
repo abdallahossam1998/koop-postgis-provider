5bd7309cf04f3e62ad503da4ff599252f558e18c
abdallahossam1998 <abdallahossam1998@google.com>
2025-09-16 01:53:36 +0300

    feat: add Swagger documentation and API docs endpoint to Koop server

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const config = require('config')
+const Koop = require('@koopjs/koop-core')
+const routes = require('./routes')
+const plugins = require('./plugins')
+const { setupSwagger } = require('./request-handlers/swagger-docs')
+
+// initiate a koop app
+const koop = new Koop()
+
+// register koop plugins
+plugins.forEach((plugin) => {
+  koop.register(plugin.instance, plugin.options)
+})
+
+// setup swagger documentation
+setupSwagger(koop.server)
+
+// add additional routes
+routes.forEach((route) => {
+  route.methods.forEach((method) => {
+    koop.server[method](route.path, route.handler)
+  })
+})
+
+// start the server
+koop.server.listen(config.port, () => {
+  koop.log.info(`Koop server listening at ${config.port}`)
+  koop.log.info(`API Documentation available at http://localhost:${config.port}/api-docs`)
+})
